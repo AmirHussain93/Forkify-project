50fb47865a4a5d66e3f362acdd38cc10a079cc58
amir.hussain <amir.hussain@mobigesture.com>
2019-02-18 16:51:06 +0530

    building the search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,32 @@
 import Search from './models/Search';
 
+//Global state of the app, search object, current recipe object, shopping list object, liked recipes
+
+const state ={};
+
 const search = new Search('pizza');
-search.getResult();
+search.getResults();
+
+const controlSearch = async () => {
+
+    // 1. get query from view
+    const query = 'pizza' //ToDo
+
+    if (query) {
+        //2. new search object and add to state
+        state.search = new Search(query);
+
+        //3. Prepare ui for results
+
+        //4. search for recipes
+        await state.search.getResults();
+
+        //5. render results on UI
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+    e.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,15 +5,17 @@ export default class Search {
         this.query = query;
     }
 
-    async getResult() {    // const key = '742aa2d90487b6548c73c085cfccf13e';
-    const key = '462b1cc8d4f2730081462fbc65136320';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
-        this.result = res.data.recipes;
-        // console.log(this.result)
-    } catch(error) {
-        alert(error);
-    }
+    async getResults() {    
+        
+        const key = '742aa2d90487b6548c73c085cfccf13e';
+        // const key = '462b1cc8d4f2730081462fbc65136320';
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result)
+        } catch(error) {
+            alert(error);
+        }
    }
 }
 
