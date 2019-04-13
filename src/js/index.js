e05447a5d37658f802e4b421ab205ce0f663bbdf
Amir Hussain <amirhuss@xlnx.xilinx.com>
2019-04-13 15:02:44 +0530

    Implementing persistent data with local storage

--- src/js/index.js
+++ src/js/index.js
@@ -131,8 +131,6 @@ elements.shopping.addEventListener('click', e => {
 });
 
 // Likes controller
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
 const controlLike = () => {
 	if (!state.likes) state.likes = new Likes();
 	const currentID = state.recipe.id;
@@ -166,6 +164,20 @@ const controlLike = () => {
 	}
 	likesView.toggleLikeMenu(state.likes.getNumLikes());
 }
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+	state.likes = new Likes();
+
+	// Restore likes
+	state.likes.readStorage();
+
+	// Toggle like menu button 
+	likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+	// Render the existing likes
+	state.likes.likes.forEach(like => likesView.renderLike(like));
+})
 
 // Handling recipe button clicks 
 elements.recipe.addEventListener('click', e => {
